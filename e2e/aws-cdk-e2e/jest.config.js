a2d8bdef54f19781c4c9514f9c8f1617ff99cc25
myin <minyin142p@gmail.com>
2020-06-20 09:08:43 +0200

    initial commit

---
+++ e2e/aws-cdk-e2e/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'aws-cdk-e2e',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/e2e/aws-cdk-e2e',
+};
---
+++ packages/aws-cdk/src/schematics/aws-cdk/schema.d.ts
@@ -0,0 +1,5 @@
+export interface AwsCdkSchematicSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
