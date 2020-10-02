51d4584149841e579104f16d27045b88b7dab653
myin <minyin142p@gmail.com>
2020-10-02 19:57:23 +0200

    Load npm when needed

--- packages/aws-cdk/src/builders/package/package.ts
+++ packages/aws-cdk/src/builders/package/package.ts
@@ -6,96 +6,96 @@ import { PackageBuilderSchema } from './schema';
 import { dirname, join } from 'path';
 import { createProjectGraph } from '@nrwl/workspace/src/core/project-graph';
 import {
-    calculateProjectDependencies,
-    checkDependentProjectsHaveBeenBuilt,
-    DependentBuildableProjectNode,
+	calculateProjectDependencies,
+	checkDependentProjectsHaveBeenBuilt,
+	DependentBuildableProjectNode,
 } from '@nrwl/workspace/src/utils/buildable-libs-utils';
 import { readJsonFile, toClassName } from '@nrwl/workspace';
 import * as rollup from 'rollup';
-import * as npm from 'npm';
 
 const typescript = require('rollup-plugin-typescript2');
 const copy = require('rollup-plugin-copy');
 
 function createRollUpConfig(
-    options: PackageBuilderSchema,
-    context: BuilderContext,
-    dependencies: DependentBuildableProjectNode[]
+	options: PackageBuilderSchema,
+	context: BuilderContext,
+	dependencies: DependentBuildableProjectNode[]
 ): rollup.RollupOptions {
-    const entryRoot = dirname(options.entryFile);
-    const packageJsonPath = join(entryRoot, 'package.json');
+	const entryRoot = dirname(options.entryFile);
+	const packageJsonPath = join(entryRoot, 'package.json');
 
-    const tsconfigPath = join(context.workspaceRoot, options.tsConfig);
-    const packageJson = readJsonFile(packageJsonPath);
-    const plugins = [
-        typescript({
-            check: true,
-            tsconfig: tsconfigPath,
-            tsconfigOverride: {
-                compilerOptions: {
-                    target: 'es2017', // keep asyc/await
-                },
-            },
-        }),
-        copy({
-            targets: [{ src: `${packageJsonPath}`, dest: `${options.outputPath}` }],
-        }),
-    ];
+	const tsconfigPath = join(context.workspaceRoot, options.tsConfig);
+	const packageJson = readJsonFile(packageJsonPath);
+	const plugins = [
+		typescript({
+			check: true,
+			tsconfig: tsconfigPath,
+			tsconfigOverride: {
+				compilerOptions: {
+					target: 'es2017', // keep asyc/await
+				},
+			},
+		}),
+		copy({
+			targets: [{ src: `${packageJsonPath}`, dest: `${options.outputPath}` }],
+		}),
+	];
 
-    const externalPackages = dependencies
-        .map((d) => d.name)
-        .concat(Object.keys(packageJson.dependencies || {}));
+	const externalPackages = dependencies
+		.map((d) => d.name)
+		.concat(Object.keys(packageJson.dependencies || {}));
 
-    return {
-        input: options.entryFile,
-        output: {
-            format: 'cjs', // build for nodejs
-            file: `${options.outputPath}/${context.target.project}.js`,
-            name: toClassName(context.target.project),
-        },
-        external: (id) => externalPackages.includes(id),
-        plugins,
-    };
+	return {
+		input: options.entryFile,
+		output: {
+			format: 'cjs', // build for nodejs
+			file: `${options.outputPath}/${context.target.project}.js`,
+			name: toClassName(context.target.project),
+		},
+		external: (id) => externalPackages.includes(id),
+		plugins,
+	};
 }
 
 export function runRollup(options: rollup.RollupOptions) {
-    return from(rollup.rollup(options)).pipe(
-        switchMap((bundle) => {
-            const outputOptions = Array.isArray(options.output) ? options.output : [options.output];
-            return from(Promise.all(outputOptions.map((o) => bundle.write(o))));
-        }),
-        map(() => ({ success: true }))
-    );
+	return from(rollup.rollup(options)).pipe(
+		switchMap((bundle) => {
+			const outputOptions = Array.isArray(options.output) ? options.output : [options.output];
+			return from(Promise.all(outputOptions.map((o) => bundle.write(o))));
+		}),
+		map(() => ({ success: true }))
+	);
 }
 
 export function packageBuilder(
-    options: PackageBuilderSchema,
-    context: BuilderContext
+	options: PackageBuilderSchema,
+	context: BuilderContext
 ): Observable<BuilderOutput> {
-    const projGraph = createProjectGraph();
-    const { dependencies } = calculateProjectDependencies(projGraph, context);
+	const projGraph = createProjectGraph();
+	const { dependencies } = calculateProjectDependencies(projGraph, context);
 
-    return of(checkDependentProjectsHaveBeenBuilt(context, dependencies)).pipe(
-        switchMap((built) => {
-            if (!built) return of({ success: false });
-            const config = createRollUpConfig(options, context, dependencies);
-            context.logger.info('Run rollup');
-            return runRollup(config);
-        }),
-        switchMap(() => {
-            return from<Promise<BuilderOutput>>(
-                new Promise((resolve) => {
-                    context.logger.info('Install packages');
-                    npm.load({ prefix: options.outputPath }, () => {
-                        npm.commands.install([], (err, data) => {
-                            if (err) context.logger.info(err.message);
-                            resolve({ success: err == null });
-                        });
-                    });
-                })
-            );
-        })
-    );
+	return of(checkDependentProjectsHaveBeenBuilt(context, dependencies)).pipe(
+		switchMap((built) => {
+			if (!built) return of({ success: false });
+			const config = createRollUpConfig(options, context, dependencies);
+			context.logger.info('Run rollup');
+			return runRollup(config);
+		}),
+		switchMap(() => {
+			return from<Promise<BuilderOutput>>(
+				new Promise((resolve) => {
+					context.logger.info('Install packages');
+					const npm = require('npm');
+					npm.load({ prefix: options.outputPath }, () => {
+						npm.commands.install([], (err, data) => {
+							if (err) context.logger.info(err.message);
+							resolve({ success: err == null });
+						});
+					});
+				})
+			);
+		})
+	);
 }
 
 export default createBuilder(packageBuilder);
