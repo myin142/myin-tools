37d733e1ad8496db62bb92e73407a140e2c2b853
myin <minyin142p@gmail.com>
2020-10-09 17:38:02 +0200

    aws-utils: support object

--- packages/aws-utils/src/lib/utils-aws.spec.ts
+++ packages/aws-utils/src/lib/utils-aws.spec.ts
@@ -2,97 +2,119 @@ import { toAWSAttributeMap } from '../index';
 import { fromAWSAttributeMap, toAWSAttributeMapArray, fromAWSAttributeMapArray } from './utils-aws';
 
 describe('Utils AWS', () => {
-    describe('To AWS AttributeMap', () => {
-        it('skip null', () => {
-            expect(toAWSAttributeMap(null)).toEqual(null);
-        });
+	describe('To AWS AttributeMap', () => {
+		it('skip null', () => {
+			expect(toAWSAttributeMap(null)).toEqual(null);
+		});
 
-        it('skip null value', () => {
-            expect(toAWSAttributeMap({ text: null })).toEqual({});
-        });
+		it('skip null value', () => {
+			expect(toAWSAttributeMap({ text: null })).toEqual({});
+		});
 
-        it('skip invalid type', () => {
-            expect(toAWSAttributeMap({ text: {} })).toEqual({});
-        });
+		it('map string', () => {
+			expect(toAWSAttributeMap({ text: 'Text' })).toEqual({
+				text: { S: 'Text' },
+			});
+		});
 
-        it('map string', () => {
-            expect(toAWSAttributeMap({ text: 'Text' })).toEqual({
-                text: { S: 'Text' },
-            });
-        });
+		it('map number', () => {
+			expect(toAWSAttributeMap({ num: 1 })).toEqual({
+				num: { N: '1' },
+			});
+		});
 
-        it('map number', () => {
-            expect(toAWSAttributeMap({ num: 1 })).toEqual({
-                num: { N: '1' },
-            });
-        });
+		it('map string array', () => {
+			expect(toAWSAttributeMap({ texts: ['hello', 'world'] })).toEqual({
+				texts: { SS: ['hello', 'world'] },
+			});
+		});
 
-        it('map string array', () => {
-            expect(toAWSAttributeMap({ texts: ['hello', 'world'] })).toEqual({
-                texts: { SS: ['hello', 'world'] },
-            });
-        });
+		it('map number array', () => {
+			expect(toAWSAttributeMap({ nums: [1, 2] })).toEqual({
+				nums: { NS: ['1', '2'] },
+			});
+		});
 
-        it('map number array', () => {
-            expect(toAWSAttributeMap({ nums: [1, 2] })).toEqual({
-                nums: { NS: ['1', '2'] },
-            });
-        });
-    });
+		it('map object to string', () => {
+			expect(toAWSAttributeMap({ obj: {} })).toEqual({
+				obj: { S: '{}' },
+			});
+		});
 
-    describe('From AWS AttributeMap', () => {
-        it('skip null', () => {
-            expect(fromAWSAttributeMap(null)).toEqual(null);
-        });
+		it('map object array to string array', () => {
+			expect(toAWSAttributeMap({ arr: [{}, {}] })).toEqual({
+				arr: {
+					SS: ['{}', '{}'],
+				},
+			});
+		});
+	});
 
-        it('skip null value', () => {
-            expect(fromAWSAttributeMap({ text: null })).toEqual({});
-        });
+	describe('From AWS AttributeMap', () => {
+		it('skip null', () => {
+			expect(fromAWSAttributeMap(null)).toEqual(null);
+		});
 
-        it('skip empty', () => {
-            expect(fromAWSAttributeMap({ text: {} })).toEqual({});
-        });
+		it('skip null value', () => {
+			expect(fromAWSAttributeMap({ text: null })).toEqual({});
+		});
 
-        it('map string', () => {
-            expect(fromAWSAttributeMap({ text: { S: 'Text' } })).toEqual({
-                text: 'Text',
-            });
-        });
+		it('skip empty', () => {
+			expect(fromAWSAttributeMap({ text: {} })).toEqual({});
+		});
 
-        it('map number', () => {
-            expect(fromAWSAttributeMap({ num: { N: '1' } })).toEqual({
-                num: 1,
-            });
-        });
+		it('map string', () => {
+			expect(fromAWSAttributeMap({ text: { S: 'Text' } })).toEqual({
+				text: 'Text',
+			});
+		});
 
-        it('map string array', () => {
-            expect(fromAWSAttributeMap({ texts: { SS: ['hello', 'world'] } })).toEqual({
-                texts: ['hello', 'world']
-            });
-        });
+		it('map object string', () => {
+			expect(fromAWSAttributeMap({ text: { S: `{"value": "1"}` } })).toEqual({
+				text: { value: '1' },
+			});
+		});
 
-        it('map number array', () => {
-            expect(fromAWSAttributeMap({ texts: { NS: ['1', '2'] } })).toEqual({
-                texts: [1, 2],
-            });
-        });
-    });
+		it('map number', () => {
+			expect(fromAWSAttributeMap({ num: { N: '1' } })).toEqual({
+				num: 1,
+			});
+		});
 
-    it('map array to AWS AttributeMap', () => {
-        expect(toAWSAttributeMapArray([{ text: 'Text', num: 1 }])).toEqual([
-            {
-                text: { S: 'Text' },
-                num: { N: '1' },
-            },
-        ]);
-    });
+		it('map string array', () => {
+			expect(fromAWSAttributeMap({ texts: { SS: ['hello', 'world'] } })).toEqual({
+				texts: ['hello', 'world']
+			});
+		});
 
-    it('map array from AWS AttributeMap', () => {
-        expect(fromAWSAttributeMapArray([{ text: { S: 'Text' }, num: { N: '1' } }])).toEqual([
-            {
-                text: 'Text',
-                num: 1,
-            },
-        ]);
-    });
+		it('map number array', () => {
+			expect(fromAWSAttributeMap({ texts: { NS: ['1', '2'] } })).toEqual({
+				texts: [1, 2],
+			});
+		});
+
+		it('map object string array', () => {
+			expect(fromAWSAttributeMap({ arr: { SS: [`{"value": 1}`, '{}'] } })).toEqual({
+				arr: [{ value: 1 }, {}],
+			});
+		});
+	});
+
+	it('map array to AWS AttributeMap', () => {
+		expect(toAWSAttributeMapArray([{ text: 'Text', num: 1 }])).toEqual([
+			{
+				text: { S: 'Text' },
+				num: { N: '1' },
+			},
+		]);
+	});
+
+	it('map array from AWS AttributeMap', () => {
+		expect(fromAWSAttributeMapArray([{ text: { S: 'Text' }, num: { N: '1' } }])).toEqual([
+			{
+				text: 'Text',
+				num: 1,
+			},
+		]);
+	});
 });
--- packages/aws-utils/src/lib/utils-aws.ts
+++ packages/aws-utils/src/lib/utils-aws.ts
@@ -2,70 +2,83 @@ import { } from 'aws-sdk';
 import { AttributeMap, AttributeValue } from 'aws-sdk/clients/dynamodb';
 
 class AWSAttributeMapAdapter {
-    static toAttributeValue(value: any): AttributeValue {
-        switch (typeof value) {
-            case 'string':
-                return { S: value };
-            case 'number':
-                return { N: `${value}` };
-            case 'object':
-                if (Array.isArray(value) && value.length > 0) {
-                    switch (typeof value[0]) {
-                        case 'string': return { SS: value };
-                        case 'number': return { NS: value.map(n => `${n}`) };
-                    }
-                }
-        }
-    }
+	static toAttributeValue(value: any): AttributeValue {
+		if (value == null) return null;
 
-    static fromAttributeValue(value: AttributeValue): any {
-        if (value == null) return null;
+		switch (typeof value) {
+			case 'string':
+				return { S: value };
+			case 'number':
+				return { N: `${value}` };
+			case 'object':
+				if (Array.isArray(value) && value.length > 0) {
+					switch (typeof value[0]) {
+						case 'string': return { SS: value };
+						case 'number': return { NS: value.map(n => `${n}`) };
+						case 'object': return { SS: value.map(s => JSON.stringify(s)) };
+					}
+				} else {
+					return { S: JSON.stringify(value) };
+				}
+		}
+	}
 
-        const type = Object.keys(value)[0];
-        switch (type) {
-            case 'S':
-            case 'SS':
-                return value[type];
-            case 'N': return parseInt(value[type]);
-            case 'NS': return value[type].map(n => parseInt(n));
-        }
-    }
+	static fromAttributeValue(value: AttributeValue): any {
+		if (value == null) return null;
+
+		const type = Object.keys(value)[0];
+		switch (type) {
+			case 'S':
+				return this.parseOptionalObject(value[type]);
+			case 'SS':
+				return value[type].map(v => this.parseOptionalObject(v));
+			case 'N': return parseInt(value[type]);
+			case 'NS': return value[type].map(n => parseInt(n));
+		}
+	}
+
+	private static parseOptionalObject(s: string): any {
+		if (s.startsWith('{') && s.endsWith('}')) {
+			return JSON.parse(s);
+		}
+		return s;
+	}
 }
 
 export const toAWSAttributeMap = <T>(obj: T): AttributeMap => {
-    if (obj == null) return null;
+	if (obj == null) return null;
 
-    const attributeMap = {};
+	const attributeMap = {};
 
-    Object.keys(obj).forEach((key) => {
-        const value = AWSAttributeMapAdapter.toAttributeValue(obj[key]);
-        if (value) {
-            attributeMap[key] = value;
-        }
-    });
+	Object.keys(obj).forEach((key) => {
+		const value = AWSAttributeMapAdapter.toAttributeValue(obj[key]);
+		if (value) {
+			attributeMap[key] = value;
+		}
+	});
 
-    return attributeMap;
+	return attributeMap;
 };
 
 export const toAWSAttributeMapArray = (arr: Array<object>): AttributeMap[] => {
-    return arr.map(toAWSAttributeMap);
+	return arr.map(toAWSAttributeMap);
 };
 
 export const fromAWSAttributeMap = <T>(attributeMap: AttributeMap): T => {
-    if (attributeMap == null) return null;
+	if (attributeMap == null) return null;
 
-    const result = {};
+	const result = {};
 
-    Object.keys(attributeMap).forEach((key) => {
-        const value = AWSAttributeMapAdapter.fromAttributeValue(attributeMap[key]);
-        if (value) {
-            result[key] = value;
-        }
-    });
+	Object.keys(attributeMap).forEach((key) => {
+		const value = AWSAttributeMapAdapter.fromAttributeValue(attributeMap[key]);
+		if (value) {
+			result[key] = value;
+		}
+	});
 
-    return result as T;
+	return result as T;
 };
 
 export const fromAWSAttributeMapArray = <T>(arr: Array<AttributeMap>): T[] => {
-    return arr.map<T>(fromAWSAttributeMap);
+	return arr.map<T>(fromAWSAttributeMap);
 };
