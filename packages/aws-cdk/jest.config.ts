85c32d81789c66b107cc03f481a82f5cf2a7fcd5
Min Yin <min.yin@evolit.com>
2022-08-18 11:02:01 +0200

    Update NX libraries to 14

--- packages/aws-cdk/jest.config.ts
+++ packages/aws-cdk/jest.config.ts
@@ -1,10 +1,11 @@
-module.exports = {
+/* eslint-disable */
+export default {
     preset: '../../jest.preset.js',
     transform: {
         '^.+\\.[tj]sx?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-    coverageDirectory: '../../coverage/packages/utils',
-    displayName: 'utils',
+    coverageDirectory: '../../coverage/packages/aws-cdk',
+    displayName: 'aws-cdk',
     testEnvironment: 'node',
 };
--- packages/utils/jest.config.ts
+++ packages/utils/jest.config.ts
@@ -1,10 +1,11 @@
-module.exports = {
+/* eslint-disable */
+export default {
     preset: '../../jest.preset.js',
     transform: {
         '^.+\\.[tj]sx?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-    coverageDirectory: '../../coverage/packages/aws-cdk',
-    displayName: 'aws-cdk',
+    coverageDirectory: '../../coverage/packages/utils',
+    displayName: 'utils',
     testEnvironment: 'node',
 };
